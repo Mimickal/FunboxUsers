98553899b45d9dc1f70c57b6692d646b664b10fb
Mimickal <mimickal.dev@gmail.com>
2018-08-26 18:29:37 -0400

    Unpack user row to object explicitly instead of using implicit tuple unpacking

--- db.py
+++ db.py
@@ -13,10 +13,24 @@ DB_CONN.executescript(SETUP_SCRIPT)
 def getUser(name):
 	'''Gets a user by their name.'''
 	global DB_CONN
+	cursor = DB_CONN.execute('SELECT * FROM Users WHERE name = ?', name)
+	row = cursor.fetchone()
 
-	cursor = DB_CONN.cursor()
-	cursor.execute('SELECT * FROM Users WHERE name = ?', name)
-	data = cursor.fetchone()
+	# Convert row to map with named fields
+	data = {}
+	desc = cursor.description
+	for x in range(len(desc)):
+		data[desc[x][0]] = row[x];
 
-	return User(*data)
+	return User(
+		id = data['id'],
+		name = data['name'],
+		pass_hash = data['pass_hash'],
+		pass_salt = data['pass_salt'],
+		email = data['email'],
+		created_at = data['created_at'],
+		updated_at = data['updated_at'],
+		accessed_at = data['accessed_at']
+	)
+
 
