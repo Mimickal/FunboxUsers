998b62b71b1a19ab0950904ccf57263ffbba95e3
Michelle <michelle@michelle-vm>
2019-07-04 02:44:30 +0200

    -Updated the setup script so it installs the right requirements on Debian based systems. -Added an option to install the requirements for tests.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
-apt-get install build-essential libssl-dev python-dev libapache2-mod-wsgi
+echo "Installing all the requirements for FunboxUsers."
+
+apt-get install build-essential libssl-dev python3 python3-dev libapache2-mod-wsgi python3-pip
 pip3 install -r requirements.txt
+
+if [ -z "$1" ] # Check if there actually was an arg
+then
+    echo "Add the command line option --test if you want to install the requirements for the tests."
+    exit 0
+else
+    TEST="--test"
+    if [ $1 = $TEST ] # Check if it matches the --test arg.
+    then
+        echo "Installing all the requirements for the tests."
+        pip3 install -r test-requirements.txt
+    else
+        echo "Add the command line option --test if you want to install the requirements for the tests."
+    fi
+fi
