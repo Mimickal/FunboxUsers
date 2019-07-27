470606fc0ccf60f8c81aa244296900111728e116
Mia Moretti <mimickal.dev@gmail.com>
2019-07-27 02:16:00 -0400

    The initial work on the account page

--- server.py
+++ server.py
@@ -144,6 +144,12 @@ def logout():
 	return ok()
 
 
+@app.route('/account', methods=['GET'])
+def getAccount():
+	csrf.protect()
+	return render_template('account.html');
+
+
 @app.route('/user', methods=['GET'])
 def getUser():
 	login_code = session.get('login', None)
---
+++ static/account.js
@@ -0,0 +1,229 @@
+window.onload = function() {
+	var username = byId("username");
+
+	var passRedacted = byId("pass-redacted");
+	var passChangeBtn = byId("pass-change-btn");
+	var passChangeForm = byId("pass-change-form");
+	var passChangeCancelBtn = byId("pass-change-cancel");
+	var passChangeSubmitBtn = byId("pass-change-submit");
+	var passIssue = byId("pass-issue");
+	var passSpinner = byId("pass-spinner");
+
+	var passOld = document.getElementsByName("pass_old")[0];
+	var passNew = document.getElementsByName("pass_new")[0];
+	var passNewConf = document.getElementsByName("pass_new_conf")[0];
+
+	var emailCurrent = byId("email-current");
+	var emailChangeBtn = byId("email-change-btn");
+	var emailCancelBtn = byId("email-cancel-btn");
+	var emailForm = byId("email-form");
+	var emailSubmitBtn = byId("email-submit-btn");
+	var emailIssue = byId("email-issue");
+	var emailSpinner = byId("email-spinner");
+	var emailConfSymbol = byId("email-conf");
+	var emailUnconfSymbol = byId("email-unconf");
+
+	var emailInput = document.getElementsByName("email_new")[0];
+
+	var displayEmailUnconf;
+	var displayEmailConf;
+
+	// Hide forms and spinners by default
+	hidePasswordForm();
+	hideEmailForm();
+
+	passChangeBtn.onclick = showPasswordForm;
+	passChangeCancelBtn.onclick = hidePasswordForm;
+	passChangeSubmitBtn.onclick = submitPassword;
+
+	emailChangeBtn.onclick = showEmailForm;
+	emailCancelBtn.onclick = hideEmailForm;
+	emailSubmitBtn.onclick = submitEmail;
+
+	function hidePasswordForm() {
+		show(passRedacted);
+		show(passChangeBtn);
+		hide(passChangeForm);
+		hide(passSpinner);
+		passIssue.textContent = "";
+
+		clear(passOld);
+		clear(passNew);
+		clear(passNewConf);
+	}
+
+	function showPasswordForm() {
+		hide(passRedacted);
+		hide(passChangeBtn);
+		show(passChangeForm);
+	}
+
+	function disablePasswordForm() {
+		passNew.setAttribute("class", null);
+		passNewConf.setAttribute("class", null);
+		passIssue.textContent = "";
+
+		disable(passOld);
+		disable(passNew);
+		disable(passNewConf);
+		disable(passChangeSubmitBtn);
+		disable(passChangeCancelBtn);
+		show(passSpinner);
+	}
+
+	function enablePasswordForm() {
+		enable(passOld);
+		enable(passNew);
+		enable(passNewConf);
+		enable(passChangeSubmitBtn);
+		enable(passChangeCancelBtn);
+		hide(passSpinner);
+	}
+
+	function submitPassword() {
+		if (passNew.value !== passNewConf.value) {
+			passNew.setAttribute("class", "issue");
+			passNewConf.setAttribute("class", "issue");
+			passIssue.textContent = "Passwords don't match!"
+		}
+		else if (passNew.value === "") {
+			passNew.setAttribute("class", "issue");
+			passNewConf.setAttribute("class", "issue");
+			passIssue.textContent = "New password is empty!";
+		}
+		else {
+			disablePasswordForm();
+			post('./', {
+				password_new: passNew.value
+			})
+			.then(function(res) {
+				hidePasswordForm();
+				enablePasswordForm();
+				passRedacted.textContent = "Password updated";
+				passRedacted.setAttribute("class", "green");
+			})
+			.catch(function(err) {
+				enablePasswordForm();
+				passIssue.textContent = err;
+			});
+		}
+	}
+
+	function hideEmailForm() {
+		hide(emailSpinner);
+		hide(emailForm);
+		enable(emailInput);
+		enable(emailSubmitBtn);
+		enable(emailCancelBtn);
+		show(emailChangeBtn);
+		show(emailCurrent);
+		clear(emailInput);
+		emailIssue.textContent = "";
+
+		emailUnconfSymbol.style.display = displayEmailUnconf;
+		emailConfSymbol.style.display = displayEmailConf;
+	}
+
+	function showEmailForm() {
+		hide(emailCurrent);
+		hide(emailChangeBtn);
+		show(emailForm);
+
+		// Flask will determine if these are shown by default
+		displayEmailConf = emailConfSymbol.style.display;
+		displayEmailUnconf = emailUnconfSymbol.style.display;
+
+		hide(emailUnconfSymbol);
+		hide(emailConfSymbol);
+	}
+
+	function disableEmailForm() {
+		emailInput.setAttribute("class", null);
+		disable(emailInput);
+		disable(emailSubmitBtn);
+		disable(emailCancelBtn);
+		show(emailSpinner);
+	}
+
+	function enableEmailForm() {
+		enable(emailInput);
+		enable(emailSubmitBtn);
+		enable(emailCancelBtn);
+		hide(emailSpinner);
+	}
+
+	function submitEmail() {
+		if (validEmail(emailInput.value)) {
+			disableEmailForm();
+			emailIssue.textContent = "";
+
+			post('./', {
+				email: emailInput.value
+			}).then(function(res) {
+				hideEmailForm();
+				emailCurrent.textContent = res.email_new;
+
+				// Both change symbols and override default display modes
+				hide(emailConfSymbol);
+				show(emailUnconfSymbol);
+				displayEmailConf = "none";
+				displayEmailUnconf = "block";
+			}).catch(function(err) {
+				enableEmailForm();
+				emailIssue.textContent = err;
+			});
+		} else {
+			emailInput.setAttribute("class", "issue");
+			emailIssue.textContent = "Invalid email";
+		}
+	}
+
+};
+
+// Shamelessly stolen from
+// https://tylermcginnis.com/validate-email-address-javascript/
+var EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function validEmail(email) {
+	return EMAIL_REGEX.test(email);
+}
+
+// TODO for browser native form backwards compatibility,
+// we may want to do 'x-www-form-urlencoded' instead.
+// TODO also, polyfill for old browsers
+// Should send with cookie named fb_session
+function post(url, data) {
+	return window.fetch(url, {
+		method: 'post',
+		headers: {
+			"Content-type": 'application/json; charset=UTF-8'
+		},
+		body: JSON.stringify(data)
+	});
+}
+
+// HELPERS
+// Abstract away all the nitty-gritty things
+function byId(id) {
+	return document.getElementById(id);
+}
+
+function hide(elem) {
+	elem.style.display = "none";
+}
+
+function show(elem) {
+	elem.style.display = "block";
+}
+
+function clear(field) {
+	field.value = "";
+	field.setAttribute("class", null);
+}
+
+function disable(elem) {
+	elem.disabled = true;
+}
+
+function enable(elem) {
+	elem.disabled = false;
+}
